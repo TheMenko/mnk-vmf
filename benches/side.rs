8b18b6f10b9465543d8375e9cda3afb1c57728de
TheMenko <themenkoprojects@gmail.com>
2025-05-15 21:08:26 +0200

    add benchmarks for side

---
+++ benches/side.rs
@@ -0,0 +1,35 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use vmf::{
+    types::Side,
+    util::{stream, tokenize},
+    Parser,
+};
+
+fn bench_side(c: &mut Criterion) {
+    let side = tokenize(
+        r#"side
+        {
+            "material" "BRICK/BRICKWALL001A"
+            "id" "42"
+            "uaxis" "[0 1 0 10] 0.125"
+            "smoothing_groups" "1"
+            "plane" "(0 0 0) (100 0 0) (100 100 0)"
+            "lightmapscale" "32"
+            "vaxis" "[1 0 0 20] 0.125"
+            "rotation" "90"
+        }"#,
+    );
+
+    c.bench_function("parse side", |b| {
+        b.iter_batched(
+            || stream(side.clone()),
+            |input| {
+                Side::parse(input).unwrap();
+            },
+            criterion::BatchSize::SmallInput,
+        )
+    });
+}
+
+criterion_group!(side_benches, bench_side);
+criterion_main!(side_benches);
